1b164635ea0574e239afe82778af5e63db50fcd0
Ivan Lin <ivan.lin1993@gmail.com>
2018-09-05 06:01:30 +0000

    fix

--- script/init.sh
+++ script/init.sh
@@ -1,4 +1,8 @@
+
+
+wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /bin/
+wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws && chmod +x heptio-authenticator-aws && mv heptio-authenticator-aws /bin/
+
+
 CLUSTER_NAME=workshop
-
-
 eksctl utils write-kubeconfig --name $CLUSTER_NAME
