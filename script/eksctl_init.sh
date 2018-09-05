d0d83e928efdc743a8e2b5fbf7942b4fff82b967
ivan <ivan>
2018-09-05 15:13:40 +0800

    fix

--- script/eksctl_init.sh
+++ script/eksctl_init.sh
@@ -1,4 +1,4 @@
-
+#!/bin/sh
 
 wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /bin/
 wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws && chmod +x heptio-authenticator-aws && mv heptio-authenticator-aws /bin/
--- script/upload_to_ecr.sh
+++ script/upload_to_ecr.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "Upload to ECR"
 
 $(aws ecr get-login --no-include-email --region us-west-2)
