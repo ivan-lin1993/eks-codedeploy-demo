832a30f05f398f8c4600210da6e5540ba574067d
ivan <ivan>
2018-09-05 15:18:08 +0800

    fix

--- script/eksctl_init.sh
+++ script/eksctl_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /bin/
-wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws && chmod +x heptio-authenticator-aws && mv heptio-authenticator-aws /bin/
+wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /usr/bin/
+wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws && chmod +x heptio-authenticator-aws && mv heptio-authenticator-aws /usr/bin/
 
 
 CLUSTER_NAME=workshop
