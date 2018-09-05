1191eba7e1afaa3b8f6e283635e7259fde3047d5
ivan <ivan>
2018-09-05 15:33:52 +0800

    fix

--- script/eksctl_init.sh
+++ script/eksctl_init.sh
@@ -4,7 +4,7 @@ wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/a
 wget https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws && chmod +x heptio-authenticator-aws && mv heptio-authenticator-aws /usr/bin/
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/bin
+mv /tmp/eksctl /usr/bin
 
 CLUSTER_NAME=workshop
 eksctl utils write-kubeconfig --name $CLUSTER_NAME
