912ecdef2f11442f74e72f1fb2ccd7b8b08bd93d
ivan <ivan>
2018-09-07 17:30:31 +0800

    fix

--- script/eksctl_init.sh
+++ script/eksctl_init.sh
@@ -10,7 +10,7 @@ eksctl utils write-kubeconfig --name $CLUSTER_NAME
 
 # echo "Setting kubectl Auth"
 curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/aws-auth-cm.yaml
-CODEBUILD_ROLE_ARN_TEMP=$(echo $CODEBUILD_ROLE_ARN | sed 's/\//\\\//')
+CODEBUILD_ROLE_ARN_TEMP=$(echo $ROLE_ARN_CODEBUILD | sed 's/\//\\\//')
 NODE_ROLE_ARN_TEMP=$(echo $NODE_ROLE_ARN | sed 's/\//\\\//')
 cat ./script/codebuild_credential.yaml | sed "s/<CODEBUILD_ROLEARN>/$CODE_BUILD_ROLE_ARN_TEMP/; s/<NODE_ROLEARN>/<$NODE_ROLE_ARN_TEMP>/" > new_aws_cm.yaml
 
