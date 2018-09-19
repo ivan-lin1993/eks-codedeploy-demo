7da6393c715d0cf8f4f29f462e0513e45c8d8598
ivan <ivan>
2018-09-19 18:27:13 +0800

    fix dockerfile

---
+++ app/nginx.conf.d
@@ -0,0 +1,12 @@
+server {
+    listen       80;
+    server_name  localhost;
+
+    #charset koi8-r;
+    #access_log  /var/log/nginx/host.access.log  main;
+
+    location / {
+        root   /var/www/html;
+        index  index.html index.htm;
+    }
+}
