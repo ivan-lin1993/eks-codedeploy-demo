520e28d4f5cd014f87148aa20e69030f5443710a
Ivan Lin <ivan.lin1993@gmail.com>
2018-10-03 10:21:01 +0000

    fix

--- script/invoke_lambda.sh
+++ script/invoke_lambda.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
+echo "aws lambda invoke --function-name $LAMBDA_NAME --payload {\"cluster_name\":\"$CLUSTER_NAME\"} ./outfile"
 aws lambda invoke --function-name $LAMBDA_NAME --payload '{"cluster_name":"'$CLUSTER_NAME'"}' ./outfile
