c35327ef18a5c47ace9f545dcf084d5fccc3e4d1
Ivan Lin <ivan.lin1993@gmail.com>
2018-10-08 05:29:01 +0000

    version control

--- script/upload_to_ecr.sh
+++ script/upload_to_ecr.sh
@@ -2,5 +2,5 @@
 echo "Upload to ECR"
 
 $(aws ecr get-login --no-include-email --region us-west-2)
-docker tag eks-web:latest 091393925708.dkr.ecr.us-west-2.amazonaws.com/eks-workshop:latest
-docker push 091393925708.dkr.ecr.us-west-2.amazonaws.com/eks-workshop:latest
+docker tag eks-web:latest 091393925708.dkr.ecr.us-west-2.amazonaws.com/eks-workshop:1
+docker push 091393925708.dkr.ecr.us-west-2.amazonaws.com/eks-workshop:1
